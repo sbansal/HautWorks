639dfe058fdd1049d7d358aef247108f3cee5841
sbansal <shubhambansal15@gmail.com>
2011-04-20 22:50:06 -0500

    Initial design for hautworks

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  
+  layout 'application'
+  def index
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hautworks::Application.config.secret_token = '5a16738925db75ed9d21934d52dea16512778c3e5f1a6ee47591ac0dc40af2e310b9857888fa426f02edfa22daf23b2f90c1259189a4b80fe11e70a3eb956ad7'
